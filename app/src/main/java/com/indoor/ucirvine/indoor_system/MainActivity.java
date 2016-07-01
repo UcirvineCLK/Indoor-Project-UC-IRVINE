1c3ab8b74b845922c49f3e5f0799fc714cf2bcb6
cwaw72 <cwaw@naver.com>
2016-06-30 23:19:18 -0700

    print uuid

--- app/src/main/java/com/indoor/ucirvine/indoor_system/MainActivity.java
+++ app/src/main/java/com/indoor/ucirvine/indoor_system/MainActivity.java
@@ -9,12 +9,20 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.indoor.ucirvine.indoor_system.view.Adapter_Rssi;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.indoor.ucirvine.indoor_system.MainActivity.ByteArrayToString;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -146,7 +154,8 @@ public class MainActivity extends AppCompatActivity {
                             }
 
                             Log.e("rssi","rssi = " +" name : " + device.getName() + " address" + device.getAddress() + " " + rssi + "time " + ts + "txpw " + txpw + " " + scanRecord);
-                            Log.e("scanRecord","scanRecord = " +                             scanRecord.toString());
+                            Log.e("scanRecord","scanRecord = " + scanRecord.toString());
+                            printScanRecord(scanRecord);
                             adapter.addItem(device.getName(),device.getAddress(), ""+rssi ,ts );
                             adapter.notifyDataSetChanged();
                         }
@@ -165,4 +174,79 @@ public class MainActivity extends AppCompatActivity {
         return file_string;
     }
 
+    public void printScanRecord (byte[] scanRecord) {
+
+        // Simply print all raw bytes
+        try {
+            String decodedRecord = new String(scanRecord,"UTF-8");
+            Log.d("DEBUG","decoded String : " + ByteArrayToString(scanRecord));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        // Parse data bytes into individual records
+        List<AdRecord> records = AdRecord.parseScanRecord(scanRecord);
+
+
+        // Print individual records
+        if (records.size() == 0) {
+            Log.i("DEBUG", "Scan Record Empty");
+        } else {
+            Log.i("DEBUG", "Scan Record: " + TextUtils.join(",", records));
+        }
+
+    }
+
+    public static String ByteArrayToString(byte[] ba)
+    {
+        StringBuilder hex = new StringBuilder(ba.length * 2);
+        for (byte b : ba)
+            hex.append(b + " ");
+
+        return hex.toString();
+    }
+
+    public static class AdRecord {
+
+        public AdRecord(int length, int type, byte[] data) {
+            String decodedRecord = "";
+            try {
+                decodedRecord = new String(data,"UTF-8");
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            Log.d("DEBUG", "Length: " + length + " Type : " + type + " Data : " + ByteArrayToString(data));
+        }
+
+        // ...
+
+        public static List<AdRecord> parseScanRecord(byte[] scanRecord) {
+            List<AdRecord> records = new ArrayList<AdRecord>();
+
+            int index = 0;
+            while (index < scanRecord.length) {
+                int length = scanRecord[index++];
+                //Done once we run out of records
+                if (length == 0) break;
+
+                int type = scanRecord[index];
+                //Done if our record isn't a valid type
+                if (type == 0) break;
+
+                byte[] data = Arrays.copyOfRange(scanRecord, index+1, index+length);
+
+                records.add(new AdRecord(length, type, data));
+                //Advance
+                index += length;
+            }
+
+            return records;
+        }
+    }
+
+
+
 }
+
